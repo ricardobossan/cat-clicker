8839458d66b01d6804a4333839e3b9db532a65eb
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-07-11 01:42:50 -0300

    add breakpoints and have 10 specs (7, green. 3, pending)

--- js/app.js
+++ js/app.js
@@ -9,19 +9,41 @@
  */
 
 /**
- * Generates the DOM
- *  @function
+ * @desc Encapsulated the whole code for the tests inside a `setTimeout()` method, with an empty delay, so that the DOM would already have loaded when
  */
-const generate = (() => {
-	const mainDiv = document.createElement('main');
-	let catDivs = [];
-	let catFigures = [];
-	for(let i = 0; i < 2; i++) {
-		catDivs.push(document.createElement('div'));
-		catFigures.push(`<figure><figcaption>cat ${i+1}</figcaption><img src="images/cat${i+1}.jpg" alt="cat ${i+1}"/></figure>`);
-		mainDiv.appendChild(catDivs[i]);
-		mainDiv.children[i].innerHTML = catFigures[i];
-	}
-	const fragment = document.createDocumentFragment();
-	fragment.appendChild(mainDiv);
-})();
+
+setTimeout(function() {
+	/**
+	 * Generates the DOM
+	 *  @function
+	 */
+	const generate = (() => {
+		const styleTag = document.createElement('style');
+		styleTag.innerHTML = '@media screen and (min-width: 960px) {main {flex-direction: row;}.catDiv {width: 40%; display: flex; align-content: space-around;}}';
+		document.head.appendChild(styleTag);
+		const bodyDom = document.querySelector('body');
+		const fragment = document.createDocumentFragment();
+		const mainDiv = document.createElement('main');
+		fragment.appendChild(mainDiv);
+		mainDiv.setAttribute("style", "display: flex; justify-content: space-around; flex-wrap: wrap; width: 100%;");
+		let catDivs = [];
+		let catFigures = [];
+		for(let i = 0; i < 2; i++) {
+			catDivs.push(document.createElement('div'));
+			catFigures.push(`<figure><figcaption>Cat ${i+1}</figcaption><img src="images/cat${i+1}.jpg" alt="Cat ${i+1}"/></figure>`);
+			mainDiv.appendChild(catDivs[i]);
+			mainDiv.children[i].innerHTML = catFigures[i];
+			catDivs[i].classList.add('catDiv');
+		}
+		bodyDom.appendChild(fragment);
+		bodyDom.setAttribute("style", "width: 100%;");
+		const imgs = document.querySelectorAll('img');
+		imgs.forEach(function(img) {
+			img.setAttribute("style", "width: 100%; height: 90%");
+		});
+		const catNames = document.querySelectorAll('figcaption');
+		catNames.forEach(function(name) {
+			name.style.textAlign = 'center';
+		});
+	})();
+});
--- spec/spec.js
+++ spec/spec.js
@@ -14,44 +14,51 @@
 setTimeout(function() {
 	(function() {
 		describe('All code to be developed will be put in this suite, which will contain all the others', () => {
+			const styleTag = document.createElement('style');
+			styleTag.innerHTML = '@media screen and (min-width: 960px) {main {flex-direction: row;}.catDiv {width: 40%; display: flex; align-content: space-around;}}';
+			document.head.appendChild(styleTag);
+			const bodyDom = document.querySelector('body');
+			const fragment = document.createDocumentFragment();
 			const mainDiv = document.createElement('main');
-			mainDiv.setAttribute("style", "display: flex; flex-direction: column; flex-wrap: wrap; width: 100%;");
+			fragment.appendChild(mainDiv);
+			mainDiv.setAttribute("style", "display: flex; justify-content: space-around; flex-wrap: wrap; width: 100%;");
 			let catDivs = [];
 			let catFigures = [];
 			for(let i = 0; i < 2; i++) {
 				catDivs.push(document.createElement('div'));
-				catFigures.push(`<figure><figcaption>cat ${i+1}</figcaption><img src="images/cat${i+1}.jpg" alt="cat ${i+1}"/></figure>`);
+				catFigures.push(`<figure><figcaption>Cat ${i+1}</figcaption><img src="images/cat${i+1}.jpg" alt="Cat ${i+1}"/></figure>`);
 				mainDiv.appendChild(catDivs[i]);
 				mainDiv.children[i].innerHTML = catFigures[i];
+				catDivs[i].classList.add('catDiv');
 			}
-			const fragment = document.createDocumentFragment();
-			fragment.appendChild(mainDiv);
-			const bodyDom = document.querySelector('body');
-			bodyDom.setAttribute("style", "width: 100%;");
 			bodyDom.appendChild(fragment);
-
-			for(let j = 0; j <= 1; j++){
-				document.querySelectorAll('figure')[j].setAttribute("style", "width: 80%;");
-				console.log(document.querySelectorAll('figure')[j]);
-			}
-
+			bodyDom.setAttribute("style", "width: 100%;");
+			const imgs = document.querySelectorAll('img');
+			imgs.forEach(function(img) {
+				img.setAttribute("style", "width: 100%; height: 90%");
+			});
+			const catNames = document.querySelectorAll('figcaption');
+			catNames.forEach(function(name) {
+				name.style.textAlign = 'center';
+			});
 			describe('generates two cat pictures, by', () => {
 				it('creating a mainDiv', () => {
 					expect(mainDiv.outerHTML).toContain('main');
 				});
 				it('creating two divs inside the mainDiv', () => {
-					expect(mainDiv.children[0].outerHTML).toContain('<div>');
-					expect(mainDiv.children[1].outerHTML).toContain('<div>');
+					expect(mainDiv.children[0].outerHTML).toContain('<div');
+					expect(mainDiv.children[1].outerHTML).toContain('<div');
 				});
 				it('making each div contain a <figure> for each cat pic', () => {
 					expect(mainDiv.children[0].outerHTML).toContain('<figure');
 					expect(mainDiv.children[1].outerHTML).toContain('<figure');
 				});
 				it('each <figure> should have inside, in this order, a <figcaption>, displaying the that cat\'s name, and an <img src="" alt="">', () => {
-					expect(mainDiv.children[0].children[0].firstChild.outerHTML).toContain('<figcaption>');
-					expect(mainDiv.children[1].children[0].firstChild.outerHTML).toContain('<figcaption>');
+					expect(mainDiv.children[0].children[0].firstChild.outerHTML).toContain('<figcaption');
+					expect(mainDiv.children[1].children[0].firstChild.outerHTML).toContain('<figcaption');
 					expect(mainDiv.children[0].children[0].lastChild.outerHTML).toContain('<img');
 					expect(mainDiv.children[1].children[0].lastChild.outerHTML).toContain('<img');
+					expect(catNames[0,1].style.textAlign).toBe('center');
 				});
 				it('creating a document fragment', () => {
 					expect(fragment.toString()).toBe('[object DocumentFragment]');
@@ -65,12 +72,9 @@ setTimeout(function() {
 					expect(document.querySelector('body').style.width).toEqual('100%');
 					expect(mainDiv.style.width).toBe('100%');
 					expect(mainDiv.style.display).toBe('flex');
-					expect(mainDiv.style.flexDirection).toBe('column');
 					expect(mainDiv.style.flexWrap).toBe('wrap');
-					expect(document.querySelectorAll('figure')[0,1].style.width).toBe('80%');
+					expect(document.querySelectorAll('img')[0,1].style.height).toBe('90%');
 				});
-				it('when page width < 960px');
-				it('but cat pictures are side by side, when >= 960px');
 			});
 			xdescribe('clicking each cat picture', () => {
 				it('increases the number of clicks at the counter', () => {
@@ -84,26 +88,5 @@ setTimeout(function() {
 				});
 			});
 		});
-
 	})();
 });
-
-/*		const domBody = document.querySelector('body');
-		const mainDiv = document.createElement('main');
-		let catDiv = [];
-		for(let i = 0; i < 2; i++) {
-			catDiv[i] = document.createElement('div');
-			mainDiv.appendChild(catDiv[i]);
-			catDiv[i].innerHTML = `<figure><figcaption>Cat ${i+1}</figcaption><img src="images/cat${i+1}.jpg" alt="cat ${i+1}" /></figure>`;
-		}
-		const fragment = document.createDocumentFragment();
-
-		domBody.setAttribute("style", 'margin: auto; width: 100%');
-
-		expect(fragment).not.toBe(undefined);
-		expect(domBody).toEqual(document.getElementsByTagName('body')[0]);
-		expect(mainDiv.children.length).toEqual(2);
-		expect(mainDiv.firstChild.firstChild).not.toBe(undefined);
-	});
-});
-*/
