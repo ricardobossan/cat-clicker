66d633da8ff250aaa585167654a9b09692d6586e
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-09-03 16:11:00 -0300

    updates code, before mergin branch to master

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /**
- * @file this file holds the logic for implementing a cat clicker page, where, once you click in a catr picture, a counter above it increments the number of clicks on that picture by 1.
+ * @file THolds the logic for implementing a cat clicker page, where, once you click in a catr picture, a counter above it increments the number of clicks on that picture by 1.
  * @author Ricardo Bossan <ricardobossan@gmail.com>
  *
  * @see [lesson: FEND Nanodegree / Chapter 4: Front-End Applications / Lesson 1: Changing Expectations](https://classroom.udacity.com/nanodegrees/nd001/parts/e4e4c31c-2348-4382-826f-caac197d6f5f/modules/4db0b091-fc81-40c2-b7f0-a4ded06480e1/lessons/3417188540/concepts/34408790480923)
