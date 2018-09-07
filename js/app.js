7caeb34c48fba1178bdca7bdeeaa8bff68b696c6
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-09-07 16:15:50 -0300

    save before loading jasmine examples on nodejs

--- js/app.js
+++ js/app.js
@@ -1,24 +1,12 @@
 /**
- * @file THolds the logic for implementing a cat clicker page, where, once you click in a catr picture, a counter above it increments the number of clicks on that picture by 1.
+ * @file THolds the logic for Project Cat Clicker Premium Pro.
  * @author Ricardo Bossan <ricardobossan@gmail.com>
  *
- * @see [lesson: FEND Nanodegree / Chapter 4: Front-End Applications / Lesson 1: Changing Expectations](https://classroom.udacity.com/nanodegrees/nd001/parts/e4e4c31c-2348-4382-826f-caac197d6f5f/modules/4db0b091-fc81-40c2-b7f0-a4ded06480e1/lessons/3417188540/concepts/34408790480923)
  */
-
-/**
- * @desc Encapsulated the whole code for the tests inside a `setTimeout()` method, with an empty delay, so that the DOM would already have loaded when
- */
-
-/*setTimeout(function() {
-*/	/**
-	 * Generates the DOM
-	 *  @function
-	 */
 /**
  * @name MODEL
  * @desc Holds the project's data
  */
-console.log(document.querySelector('p'));
 let model = {
 	cats: [
 		ifrit = {
@@ -58,8 +46,8 @@ const octopus = {
 	init: function() {
 
 		view.init();
-/*		view.event();
-*/	},
+		view.event();
+	},
 
 	hideOrRevealForm: function() {
 
@@ -77,15 +65,15 @@ const octopus = {
 const view = {
 
 	init: function() {
-		this.adminButton = document.querySelector('.admin');
-		this.newCatForm = document.querySelector('.new-cat-form');
+		this.adminButton = document.querySelector('#admin');
+		this.newCatForm = document.querySelector('#new-cat-form');
 	},
-/*	event: function() {
+	event: function() {
 		this.adminButton.addEventListener("click", function() {
 			octopus.hideOrRevealForm();
 		});
 	}
-*/};
+};
 
 octopus.init();
 console.log(view.newCatForm);
